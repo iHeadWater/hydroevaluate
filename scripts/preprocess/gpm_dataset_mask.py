37f3e66b758a59944d028de76a04a359ac40a5bd
silencesoup <50292487+silencesoup@users.noreply.github.com>
2023-11-01 11:05:15 +0800

    fix some bugs

--- scripts/preprocess/gpm_dataset_mask.py
+++ scripts/preprocess/gpm_dataset_mask.py
@@ -19,115 +19,69 @@ watershed = geopandas.read_file('../data/671_shp/671-hyd_na_dir_30s.shp')
 # gen_mask(watershed, "STAID", "gpm", save_dir="./mask")
 
 # 设置文件夹路径
-mask_folder_path = './mask_GPM/'
-gpm_folder_path = '../data/GPM/'
+mask_folder_path = '/path_to_mask/'
+gpm_folder_path = '/path_to_gpm/'
+output_base_path = '/path_to_output_folder/'
 
 # 获取文件夹中所有的文件
 all_mask_files = os.listdir(mask_folder_path)
-'''
-# 正则表达式模式，用于提取数字，因为那个mask函数生成的文件名太乱了，这里要整理一下
-pattern = re.compile(r'mask-(\d{1,8})-.*\.nc')
 
-# 遍历文件，把mask重命名，名字为id
+# 定义寻找最近索引的函数
+def find_nearest_index(array, value):
+    idx = np.abs(array - value).argmin()
+    return idx
 
-for file_name in all_mask_files:
-    match = pattern.match(file_name)
-    if match:
-        # 获取匹配到的数字部分
-        number_str = match.group(1)
+# 获取.nc和.nc4文件列表
+nc_files = sorted([os.path.join(mask_folder_path, f) for f in os.listdir(mask_folder_path) if f.endswith('.nc')])
+nc4_files = sorted([os.path.join(gpm_folder_path, f) for f in os.listdir(gpm_folder_path) if f.endswith('.nc4')])
+
+# 处理每个.nc文件
+for nc_file in nc_files:
+    # 读取.nc文件数据
+    nc_data = xr.open_dataset(nc_file)
+    lon_nc = nc_data.lon.values
+    lat_nc = nc_data.lat.values
+
+    # 创建以.nc文件名命名的输出文件夹
+    nc_file_name = os.path.splitext(os.path.basename(nc_file))[0]
+    nc_output_path = os.path.join(output_base_path, nc_file_name)
+    os.makedirs(nc_output_path, exist_ok=True)
+
+    # 对每个.nc4文件进行操作
+    for nc4_file in nc4_files:
+        # 读取.nc4文件数据
+        nc4_data = xr.open_dataset(nc4_file)
+        precipitation_cal = nc4_data['precipitationCal']
+
+        # 初始化一个空的DataArray用于存储乘积结果
+        multiplied_values = np.empty_like(nc_data['w'].values)
+
+        # 对于每个经纬度点，找到.nc4中最近的点并乘以.nc中的w值
+        for i, lon in enumerate(lon_nc):
+            for j, lat in enumerate(lat_nc):
+                lon_idx = find_nearest_index(nc4_data.lon.values, lon)
+                lat_idx = find_nearest_index(nc4_data.lat.values, lat)
+                w_value = nc_data['w'].isel(lon=i, lat=j).values
+                precipitation_value = precipitation_cal.isel(lon=lon_idx, lat=lat_idx).values
+                multiplied_values[i, j] = w_value * precipitation_value
+
+        # 将乘积结果转换成DataArray
+        multiplied_data = xr.DataArray(multiplied_values, coords=[('lon', lon_nc), ('lat', lat_nc)])
+
+        # 提取时间戳，用于文件命名
+        # 假设时间数据是numpy.datetime64类型的数组
+        if isinstance(nc4_data.time.values[0], np.datetime64):
+            time_str = np.datetime_as_string(nc4_data.time.values[0], unit='s')
+        else:
+            # 如果是其他类型，可能需要进一步的处理
+            time_str = str(nc4_data.time.values[0])
         
-        # 左侧补0至8位
-        number_str_padded = number_str.zfill(8)
-        
-        # 构造新的文件名
-        new_file_name = f'{number_str_padded}.nc'
-        
-        # 获取完整的文件路径
-        old_file_path = os.path.join(mask_folder_path, file_name)
-        new_file_path = os.path.join(mask_folder_path, new_file_name)
-        
-        # 重命名文件
-        os.rename(old_file_path, new_file_path)
-        
-        # 输出或其他操作
-        print(f'Renamed: {file_name} -> {new_file_name}')
-'''
+        # 替换T为一个空格
+        time_str = time_str.replace('T', ' ')
 
-# 正则表达式模式，用于提取日期和时间信息
-pattern = re.compile(r'3B-HHR-E\.MS\.MRG\.3IMERG\.(\d{8})-S(\d{6})-E(\d{6})\.\d{4}\.V06C\.HDF5\.SUB\.nc4')
+        # 创建新的文件名
+        new_file_name = f"{time_str}.nc"
+        output_base_path = os.path.join(nc_output_path, new_file_name)
 
-# 获取文件夹中所有的文件
-all_GPM_files = os.listdir(gpm_folder_path)
-
-# 空列表，用于存储文件名和对应的时间信息
-file_times = []
-
-# 遍历文件，提取日期和时间信息
-for gpm_file_name in all_GPM_files:
-    match = pattern.match(gpm_file_name)
-    if match:
-        date_str, start_time_str, end_time_str = match.groups()
-        dt = datetime.datetime.strptime(date_str + start_time_str, '%Y%m%d%H%M%S')
-        file_times.append((gpm_file_name, dt))
-
-# 按时间排序
-file_times.sort(key=lambda x: x[1])
-
-all_mask_files = os.listdir(mask_folder_path)
-# 依次读取文件
-for mask_file_name in all_mask_files:
-    
-    try:
-        mask = xr.open_dataset(mask_folder_path + mask_file_name)
-        lon_min = float(format(mask.coords["lon"][0].values))
-        lat_min = float(format(mask.coords["lat"][0].values))
-        lon_max = float(format(mask.coords["lon"][-1].values))
-        lat_max = float(format(mask.coords["lat"][-1].values))
-
-        for gpm_time_file_name, gpm_time_file_time in file_times:
-            gpm_time_file_path = os.path.join(gpm_folder_path, gpm_time_file_name)
-
-            data = xr.open_dataset(gpm_time_file_path)
-
-            data_process = data.sel(
-                lon=slice(lon_min, lon_max + 0.01),
-                lat=slice(lat_min, lat_max + 0.01)
-            )
-
-            data_process_path = "mask_stream_data/" + mask_file_name.replace(".nc", "") + "/"
-
-            data_process_name = gpm_time_file_time
-
-            data_process_full_path = data_process_path + str(data_process_name)
-
-            # 检查路径是否存在，若不存在，则创建该路径
-            if not os.path.exists(data_process_path):
-                os.makedirs(data_process_path)
-                print(f"Path {data_process_path} created")
-            else:
-                print(f"Path {data_process_path} already exists")
-
-            data_process.to_netcdf(data_process_full_path + ".nc")
-            
-    except:
-        for gpm_time_file_name, gpm_time_file_time in file_times:
-            gpm_time_file_path = os.path.join(gpm_folder_path, gpm_time_file_name)
-
-            data = xr.open_dataset(gpm_time_file_path)
-            
-            data_process = data
-
-            data_process_path = "mask_stream_data/" + mask_file_name.replace(".nc", "") + "_error/"
-
-            data_process_name = gpm_time_file_time
-
-            data_process_full_path = data_process_path + str(data_process_name)
-
-            # 检查路径是否存在，若不存在，则创建该路径
-            if not os.path.exists(data_process_path):
-                os.makedirs(data_process_path)
-                print(f"Path {data_process_path} created")
-            else:
-                print(f"Path {data_process_path} already exists")
-
-            # data_process.to_netcdf(data_process_full_path + "_error.nc")
+        # 保存乘积数据到新文件
+        multiplied_data.to_netcdf(output_base_path)
